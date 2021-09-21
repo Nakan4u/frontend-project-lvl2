b9af953ef32aa46772ab22729ca4dbbcbf19f58e
Grygorii <nakan88@gmail.com>
2021-09-21 23:18:39 +0300

    implement task 2

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import { Command } from 'commander';
 export default () => {
   const program = new Command();
   program.version('0.0.1');
+  program.arguments('<filepath1> <filepath2>');
   program.description('Compares two configuration files and shows a difference.');
+  program.option('-f, --format <type>', 'output format');
   return program;
 }
