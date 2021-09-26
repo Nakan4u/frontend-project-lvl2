e7289dc591a415972fa90538593cb853f8167c45
Grygorii <nakan88@gmail.com>
2021-09-26 13:05:13 +0300

    fix import

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import program from 'commander';
-import { genDiff } from '../src/index.js';
+import { getDiff } from '../src/index.js';
 
 program
   .version('1.0.0')
@@ -8,7 +8,7 @@ program
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format [type]', 'output format', 'stylish')
   .action((filepath1, filepath2) => {
-    const difference = genDiff(filepath1, filepath2);
+    const difference = getDiff(filepath1, filepath2);
     console.log(difference);
   })
   .parse(process.argv);
