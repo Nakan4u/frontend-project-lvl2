0da29c8719d06db32d04a1df01a9d32c6b9f42ac
Grygorii <nakan88@gmail.com>
2021-09-26 12:57:31 +0300

    fix tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,17 +2,17 @@
 import getDiff, { readFile } from '../src/index.js';
 
 test('findDifferences JSON should return correct result', () => {
-  const file1 = readFile('file1.json');
-  const file2 = readFile('file2.json');
+  const file1Path = 'file1.json';
+  const file2Path = 'file2.json';
   const expected = readFile('files-diff.txt');
-  const diff = getDiff(file1, file2);
+  const diff = getDiff(file1Path, file2Path);
   expect(diff).toEqual(expected);
 });
 
 test('findDifferences YAML should return correct result', () => {
-  const file1 = readFile('person1.yaml');
-  const file2 = readFile('person2.yaml');
+  const file1Path = 'person1.yaml';
+  const file2Path = 'person2.yaml';
   const expected = readFile('persons-diff.txt');
-  const diff = getDiff(file1, file2);
+  const diff = getDiff(file1Path, file2Path);
   expect(diff).toEqual(expected);
 });
