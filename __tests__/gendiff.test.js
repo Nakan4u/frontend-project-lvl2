15c533fac5516cda0fd3e1f76f701a46319be7b8
Grygorii <nakan88@gmail.com>
2021-09-28 22:36:46 +0300

    add tests for deep files

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -9,10 +9,26 @@ test('findDifferences JSON should return correct result', () => {
   expect(diff).toEqual(expected);
 });
 
+test('findDifferences JSON should return correct diff with deep files', () => {
+  const file1Path = 'file3.json';
+  const file2Path = 'file4.json';
+  const expected = readFile('files-deep-diff.txt');
+  const diff = getDiff(file1Path, file2Path);
+  expect(diff).toEqual(expected);
+});
+
 test('findDifferences YAML should return correct result', () => {
   const file1Path = 'person1.yaml';
   const file2Path = 'person2.yaml';
   const expected = readFile('persons-diff.txt');
+  const diff = getDiff(file1Path, file2Path);
+  expect(diff).toEqual(expected);
+});
+
+test('findDifferences YAML should return correct diff with deep files', () => {
+  const file1Path = 'file3.yaml';
+  const file2Path = 'file4.yaml';
+  const expected = readFile('files-deep-diff.txt');
   const diff = getDiff(file1Path, file2Path);
   expect(diff).toEqual(expected);
 });
